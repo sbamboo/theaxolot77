b4cb2c976d6e19b10405562f59ea36ea47b9a5f6
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-03-02 00:43:16 +0100

    things

--- storage/serve.js
+++ storage/serve.js
@@ -9,7 +9,7 @@ function convertBytesToLargestUnit(bytesSize) {
 }
 
 // Function to fetch JSON file and download parts or output checksum
-async function processFile(url) {
+async function processFile(url,skipClick=false) {
     if (!url) {
         console.error('URL parameter is missing.');
         return;
@@ -25,6 +25,26 @@ async function processFile(url) {
         const chunks = jsonData.chunks;
         const parts = [];
 
+        // Get ui parts
+        const download_filename = document.getElementById('download-info-filen');
+        const download_filesize = document.getElementById('download-info-size');
+        const download_checksum = document.getElementById('download-info-checks');
+        const download_linkinfo = document.getElementById('download-info-url');
+
+        // Fill in known info
+        download_filename.innerHTML = download_filename.innerHTML.replace("FILENAME",filename);
+        download_filesize.innerHTML = download_filesize.innerHTML.replace("FILESIZE",convertBytesToLargestUnit(jsonData.size));
+        download_checksum.innerHTML = download_checksum.innerHTML.replace("HASH_ALGORITHM",jsonData.checksum.hash + " (" + jsonData.checksum.algorithm + ")");
+
+        if (skipClick != false) {
+            download_linkinfo.innerHTML = `Dosen't redirect? <a class="link-label" id="download-info-link">Click here</a>`;
+        } else {
+            download_linkinfo.innerHTML = `To download: <a class="link-label" id="download-info-link">Click here</a>`;
+        }
+        
+        const download_linktag  = document.getElementById('download-info-link');
+        download_linktag.innerHTML = 'Generating download link...';
+
         // Fetch each part
         for (let i = 0; i < chunks.length; i++) {
             const partUrl = chunks[i];
@@ -37,19 +57,13 @@ async function processFile(url) {
         const combinedBlob = new Blob(parts);
 
         // Create download link
-        const downloadLink = document.createElement('a');
-        const checksum = jsonData.checksum.hash;
+        download_linktag.href = URL.createObjectURL(combinedBlob);
+        download_linktag.download = filename;
+        download_linktag.innerHTML = 'Click here';
         
-        downloadLink.href = URL.createObjectURL(combinedBlob);
-        downloadLink.download = filename;
-        downloadLink.innerHTML = 'Click here to download the file';
-        
-        hashTxt = document.createElement('p');
-        hashTxt.innerHTML = 'Checksum: ' + checksum;
-        
-        document.body.appendChild(downloadLink);
-        document.body.appendChild(hashTxt);
-
+        if (skipClick != false) {
+            download_linktag.click();
+        }
 
     } catch (error) {
         console.error('Error:', error);
@@ -140,6 +154,7 @@ function showStore() {
 window.onload = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const url = urlParams.get('url');
+    const skipClick = urlParams.get('manual');
 
     const downloadDisplay = document.getElementById('download-display');
     const storeDisplay = document.getElementById('store-display');
@@ -149,8 +164,8 @@ window.onload = () => {
         storeDisplay.style.display = 'block';
         showStore();
     } else {
-        downloadDisplay.style.display = 'block';
+        downloadDisplay.style.display = 'flex';
         storeDisplay.style.display = 'none';
-        processFile(url);
+        processFile(url,skipClick);
     }
 };
