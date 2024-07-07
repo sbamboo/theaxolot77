52064ca0c87c15a077eebe7a380c86f8a9dd42aa
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-07-07 19:51:54 +0200

    update

--- articles/viewer/js/main.js
+++ articles/viewer/js/main.js
@@ -6,11 +6,30 @@ async function wrapp(docWrapper,markdown) {
 
 window.onload = () => {
     const urlParams = new URLSearchParams(window.location.search);
-    const markdown = urlParams.get('markdown');
+    const markdown = decodeURIComponent(urlParams.get('markdown'));
 
     const docWrapper = document.getElementById("doc");
 
     wrapp(docWrapper,markdown);
 
     docWrapper.removeAttribute('hidden');
+
+    const retUrlParam = urlParams.get('return-url');
+    const retUrl = decodeURIComponent(retUrlParam);
+    if (retUrlParam) {
+        const topbar = document.getElementById("topbar");
+        const returnContainer = document.createElement("div");
+        const returnBtn = document.createElement("a");
+        returnContainer.classList.add("returnbtn-wrapper");
+        returnBtn.classList.add("returnbtn");
+        if (retUrl === "history-back") {
+            returnBtn.onclick = () => { window.history.back(); };
+        } else if (retUrl && retUrl !== null && retUrl !== "null") {
+            returnBtn.href = retUrl;
+        }
+        returnBtn.innerText = "« Back";
+        returnBtn.style.fontSize = "16px"
+        returnContainer.appendChild(returnBtn);
+        topbar.appendChild(returnContainer);
+    }
 }
